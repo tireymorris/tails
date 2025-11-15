43dfd1c0af434e73b16ccc860c6cdf12a50b4afe
Tirey Morris <tirey@hey.com>
2025-11-15 14:42:04 -0600

    fix rack attack error

--- config/rack_attack.rb
+++ config/rack_attack.rb
@@ -10,10 +10,6 @@ Rack::Attack.throttle('register/ip', limit: 3, period: 300) do |req|
   req.ip if req.path == '/auth/register' && req.post?
 end
 
-Rack::Attack.throttle('login/email', limit: 5, period: 60) do |req|
-  req.params['email'].to_s.downcase.presence if req.path == '/auth/login' && req.post?
-end
-
 Rack::Attack.throttled_responder = lambda do |env|
   retry_after = (env['rack.attack.match_data'] || {})[:period]
   [
