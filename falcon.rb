2b8518ef2f2edbb0d232d91d05e7e1b7aea24436
Tirey Morris <tirey@traveljoy.com>
2025-11-16 13:32:13 -0600

    delete unused code

--- falcon.rb
+++
@@ -1,9 +0,0 @@
-load :rack, :supervisor
-
-hostname = File.basename(__dir__)
-port = ENV.fetch('PORT', 9292).to_i
-
-rack hostname do
-  cache true
-  endpoint Async::HTTP::Endpoint.parse("http://0.0.0.0:#{port}")
-end
